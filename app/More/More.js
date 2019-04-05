cc66a3b55152ca0d5719303b838b4e841ac30a26
Developer <developer@Developers-iMac.local>
2019-04-05 14:00:57 +0100

    fix align text on More fields #pulse 210177759

--- app/More/More.js
+++ app/More/More.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
-  ScrollView, View, StatusBar, Alert, Image, Linking,
+  ScrollView, View, StatusBar, Alert, Image, Linking, TouchableOpacity,
 } from 'react-native';
 import { moderateScale } from 'react-native-size-matters';
-import { ListItem } from 'react-native-elements';
+import { ListItem, Icon, Text } from 'react-native-elements';
 import OfflineMsg from '../common/OfflineMsg';
 import ShowModal from '../common/ShowModal';
 
@@ -11,31 +11,48 @@ const episodesIcon = require('../../img/episodes.png');
 
 const menu = {
   account: { 
-    title: 'My Account', navigateTo: 'Account', iconName: 'account', iconType: 'material-community',
+    title: 'My Account', navigateTo: 'Account', iconName: 'account', iconType: 'material-community', margin: 7,
   },
   credits: {
-    title: 'Credits', navigateTo: 'Credits', iconName: 'menu', iconType: 'entypo',
+    title: 'Credits', navigateTo: 'Credits', iconName: 'menu', iconType: 'entypo', margin: 7,
   },
   trailers: {
-    title: 'Trailers', navigateTo: 'Trailers', iconName: 'soundcloud', iconType: 'entypo',
+    title: 'Trailers', navigateTo: 'Trailers', iconName: 'soundcloud', iconType: 'entypo', margin: 0,
   },
   kickstarters: {
-    title: 'Kickstarter Backers', navigateTo: 'Kickstarter', iconName: 'ios-ribbon', iconType: 'ionicon',
+    title: 'Kickstarter Backers', navigateTo: 'Kickstarter', iconName: 'ios-ribbon', iconType: 'ionicon', margin: 12,
   },
   tutorial: {
-    title: 'Tutorial', navigateTo: 'Tutorial', iconName: 'question', iconType: 'font-awesome',
+    title: 'Tutorial', navigateTo: 'Tutorial', iconName: 'question', iconType: 'font-awesome', margin: 18,
   },
   tips: {
-    title: 'FAQ', navigateTo: 'Tips', iconName: 'star', iconType: 'entypo',
+    title: 'FAQ', navigateTo: 'Tips', iconName: 'star', iconType: 'entypo', margin: 5,
   },
   downloads: {
-    title: 'Manage Downloads', navigateTo: 'Downloads', iconName: 'ios-cloud-download', iconType: 'ionicon',
+    title: 'Manage Downloads', navigateTo: 'Downloads', iconName: 'ios-cloud-download', iconType: 'ionicon', margin: 8,
   },
   agreement: {
-    title: 'User Agreement', navigateTo: 'Agreement', iconName: 'check', iconType: 'entypo',
+    title: 'User Agreement', navigateTo: 'Agreement', iconName: 'check', iconType: 'entypo', margin: 5,
   },
   feedback: {
-    title: 'Feedback', navigateTo: 'Feedback', iconName: 'message-circle', iconType: 'feather',
+    title: 'Feedback', navigateTo: 'Feedback', iconName: 'message-circle', iconType: 'feather', margin: 5,
+  },
+};
+
+const styles = {
+  itemContainers: {
+    backgroundColor: '#33425a',
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: moderateScale(10),
+  },
+  titleStyle: {
+    color: 'white',
+    fontSize: moderateScale(18),
+    marginLeft: moderateScale(10),
+    flex: 0.7,
   },
 };
 
@@ -73,8 +90,8 @@ export default class More extends React.Component {
         <ListItem
           key={key}
           title={value.title}
-          titleStyle={{ color: 'white', fontSize: moderateScale(18), flex: 0.7 }}
-          containerStyle={{ backgroundColor: '#33425a', flex: 1 }}
+          titleStyle={{ color: 'white', fontSize: moderateScale(18) }}
+          containerStyle={{ backgroundColor: '#33425a' }}
           leftIcon={value.title === 'Trailers'
             ? (
               <Image
@@ -83,8 +100,7 @@ export default class More extends React.Component {
                   height: moderateScale(30),
                   width: moderateScale(30),
                   tintColor: 'white',
-                  marginRight: moderateScale(10),
-                  
+                  marginRight: moderateScale(7),
                 }}
               />
             )
@@ -93,11 +109,55 @@ export default class More extends React.Component {
               type: value.iconType,
               size: moderateScale(30),
               color: 'white',
-              style: { flex: 0.3 },
+              style: { marginRight: moderateScale(value.margin) },
             }}
           underlayColor="#2a3545"
           onPress={() => this.navigateTo(value.navigateTo)}
         />
+        // <View>
+        //   <TouchableOpacity onPress={() => this.navigateTo(value.navigateTo)}>
+        //     <View style={styles.itemContainers}>
+        //       <View style={{ flexDirection: 'row' }}>
+        //         {value.title === 'Trailers'
+        //           ? (
+        //             <Image
+        //               source={episodesIcon}
+        //               style={{
+        //                 height: moderateScale(30),
+        //                 width: moderateScale(30),
+        //                 tintColor: 'white',
+        //                 marginRight: moderateScale(10),
+        //               }}
+        //             />
+        //           )
+        //           : (
+        //             <Icon
+        //               name={value.iconName}
+        //               type={value.iconType}
+        //               size={moderateScale(30)}
+        //               color="white"
+        //               underlayColor="#2a3545"
+        //               onPress={() => this.navigateTo(value.navigateTo)}
+        //               style={{ flex: 0.3 }}
+        //             />
+        //           )
+        //         }
+        //         <Text style={styles.titleStyle}>
+        //           {value.title}
+        //         </Text>
+        //       </View>
+        //       <Icon
+        //         name="chevron-right"
+        //         type="entypo"
+        //         size={moderateScale(30)}
+        //         color="white"
+        //         underlayColor="#2a3545"
+        //         style={{ alignSelf: 'flex-end' }}
+        //       />
+        //     </View>
+        //     <View style={{ flex: 1, backgroundColor: 'white', height: 1 }} />
+        //   </TouchableOpacity>
+        // </View>
       );
     });
     return (
